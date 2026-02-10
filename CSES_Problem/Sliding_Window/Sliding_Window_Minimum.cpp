190ef4625764943a87d9969c2cfe0dffbf5d35ae
Saurob17 <mdsaurobbhuyan@gmail.com>
2026-02-10 15:48:43 +0600

    slaiding windo

---
+++ CSES_Problem/Sliding_Window/Sliding_Window_Minimum.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long n, k;
+    cin >> n >> k;
+
+    long long x, a, b, c;
+    cin >> x >> a >> b >> c;
+
+    vector<long long> v(n);
+    v[0] = x;
+
+    for (int i = 1; i < n; i++) {
+        v[i] = (v[i - 1] * a + b) % c;
+    }
+
+    deque<int> dq;
+    vector<long long> ans;
+
+    for (int i = 0; i < n; i++) {
+        // window এর বাইরে গেলে pop
+        while (!dq.empty() && dq.front() <= i - k)
+            dq.pop_front();
+
+        // বড় element বাদ দিতে হবে back থেকে
+        while (!dq.empty() && v[i] <= v[dq.back()])
+            dq.pop_back();
+
+        dq.push_back(i);
+
+        if (i >= k - 1)
+            ans.push_back(v[dq.front()]);
+    }
+
+    long long val = 0;
+    for (int i = 0; i < ans.size(); i++)
+        val ^= ans[i];
+
+    cout << val << "\n";
+
+    return 0;
+}
---
+++ CSES_Problem/Sliding_Window/Sliding_Window_Sum.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    long long k, n;
+    cin >> n >> k;
+
+    long long x, a, b, c;
+    cin >>x>> a >> b >> c;
+
+    vector<long long>v(n);
+    v[0] = x;
+
+    for (int i = 1;i < n;i++) {
+        v[i] = ((v[i - 1] * a) + b) % c;
+    }   
+
+    vector<long long>ans;
+    int l = 0, r = k ;
+    long long sum = 0;
+    for (int i = 0;i < k;i++) {
+        sum += v[i];
+    }
+
+    while (r < n)
+    {
+        /* code */
+        long long temsum = sum;
+        temsum += v[r];
+        temsum -= v[l];
+        l++;
+        r++;
+        sum ^= temsum;
+    }
+    cout << sum;
+}
